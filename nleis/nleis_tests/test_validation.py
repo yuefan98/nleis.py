cd5355562f777297ac69b17fc9afcbe278603fef
Yuefan Ji <yuefan@uw.edu>
2025-01-03 14:50:35 -0800

    Code and test improvement: * refactor LinMM ('conf') and NL_MM ('cost') to a single MM function that support 'conf' and 'cost' method * minor bug fixes * updates unit tests to reflect the changes * 100% coverage

--- nleis/nleis_tests/test_validation.py
+++ nleis/nleis_tests/test_validation.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from nleis.validation import NL_MM, LinMM, cost_max_norm
+from nleis.validation import MM, cost_max_norm
 from impedance.models.circuits import CustomCircuit
 from nleis import NLEISCustomCircuit
 import pytest
@@ -12,7 +12,7 @@ def suppress_plot_show(monkeypatch):
     monkeypatch.setattr(plt, "show", lambda: None)
 
 
-def test_NL_MM():
+def test_MM_cost_method():
 
     model = NLEISCustomCircuit('RCn', initial_guess=[.1, 1, 0.1])
     f = np.geomspace(1e-2, 1e2, 100)
@@ -20,14 +20,14 @@ def test_NL_MM():
 
     # test invalid max_M raises value error
     with pytest.raises(ValueError):
-        M, cost, p, Z_fit, res_real, res_imag, conf = NL_MM(
-            f, Z2, initial_guess=[.1, 1], tol=1e-5,
-            plot=True, max_f=10, max_M=0)
+        M, cost, p, Z_fit, res_real, res_imag, conf = MM(
+            f, Z2, raw_circuit='Kn', initial_guess=[.1, 1], method='cost',
+            max_f=10, max_M=0, tol=1e-5, plot=True)
 
     # check for an ideal weakly nonlinear RC, M equal to 1
-    M, cost, p, Z_fit, res_real, res_imag, conf = NL_MM(
-        f, Z2, initial_guess=[.1, 1], tol=1e-5,
-        plot=True, max_f=10, max_M=10)
+    M, cost, p, Z_fit, res_real, res_imag, conf = MM(
+        f, Z2, raw_circuit='Kn', initial_guess=[.1, 1], method='cost',
+        max_f=10, max_M=10, tol=1e-5, plot=True)
     assert np.isclose(M, 1)
 
     model = CustomCircuit('TPn0', initial_guess=[1, 1, 1, 0.1])
@@ -35,24 +35,27 @@ def test_NL_MM():
 
     # test optimal solution unfound with max_M raises value error
     with pytest.raises(ValueError):
-        M, cost, p, Z_fit, res_real, res_imag, conf = NL_MM(
-            f, Z2, initial_guess=[.1, 1], tol=1e-5,
-            plot=True, max_f=10, max_M=2)
+        M, cost, p, Z_fit, res_real, res_imag, conf = MM(
+            f, Z2, raw_circuit='Kn', initial_guess=[.1, 1], method='cost',
+            max_f=10, max_M=2, tol=1e-5, plot=True)
 
 
-def test_LinMM():
+def test_MM_conf_method():
     model = CustomCircuit('p(R0,C0)', initial_guess=[.1, 1])
     f = np.geomspace(1e-2, 1e2, 100)
     Z1 = model.predict(f)
 
     # test invalid max_M raises value error
     with pytest.raises(ValueError):
-        M, p, Z_fit, res_real, res_imag, conf = LinMM(
-            f, Z1, initial_guess=[.1, .1], plot=True, CI_plot=True, max_M=0)
+        M, p, Z_fit, res_real, res_imag, conf = MM(
+            f, Z1, raw_circuit='K', initial_guess=[.1, .1], method='conf',
+            max_M=0, plot=True, CI_plot=True)
 
     # check for an ideal RC, M equal to 1
-    M, p, Z_fit, res_real, res_imag, conf = LinMM(
-        f, Z1, initial_guess=[.1, .1], plot=True, CI_plot=True, max_M=10)
+    M, p, Z_fit, res_real, res_imag, conf = MM(
+        f, Z1, raw_circuit='K', initial_guess=[.1, .1], method='conf',
+        max_M=10, plot=True, CI_plot=True)
+    print(M)
     assert np.isclose(M, 1)
 
     model = CustomCircuit('TP0', initial_guess=[1, 1, 1])
@@ -61,8 +64,29 @@ def test_LinMM():
 
     # test optimal solution unfound with max_M raises value error
     with pytest.raises(ValueError):
-        M, p, Z_fit, res_real, res_imag, conf = LinMM(
-            f, Z1, initial_guess=[.1, .1], plot=True, CI_plot=True, max_M=3)
+        M, p, Z_fit, res_real, res_imag, conf = MM(
+            f, Z1, raw_circuit='K', initial_guess=[.1, .1], method='conf',
+            max_M=3, plot=True, CI_plot=True)
+
+    # test faliure of conf method raises value error
+    with pytest.raises(ValueError):
+        M, p, Z_fit, res_real, res_imag, conf = MM(
+            f, -Z1, raw_circuit='K', initial_guess=[.1, .1], method='conf',
+            max_M=3, plot=True, CI_plot=True)
+
+
+def test_MM_method():
+    # test invalid method raises value error
+    with pytest.raises(ValueError):
+        MM(np.array([1, 2, 3]), np.array([1, 2, 3]), raw_circuit='K',
+           initial_guess=[1, 1], method='invalid_method')
+    # test non-integer max_M raises TypeError for both method
+    with pytest.raises(TypeError):
+        MM(np.array([1, 2, 3]), np.array([1, 2, 3]), raw_circuit='K',
+           initial_guess=[1, 1], method='cost', max_M=1.5)
+    with pytest.raises(TypeError):
+        MM(np.array([1, 2, 3]), np.array([1, 2, 3]), raw_circuit='K',
+           initial_guess=[1, 1], method='conf', max_M=1.5)
 
 
 def test_cost_max_norm():
--- nleis/validation.py
+++ nleis/validation.py
@@ -1,40 +1,127 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from impedance.models.circuits import CustomCircuit
 from .nleis import NLEISCustomCircuit
 from .visualization import plot_first, plot_second
 from tqdm import tqdm
 
 
-def NL_MM(f, Z, raw_circuit='Kn', initial_guess=[0.01, 1], tol=1e-5,
-          plot=False, max_f=10, max_M=20):
-    """
-    Perform NLEIS fitting using nonlinear measurement models
-    with a specified maximum number of elements.
+def MM(f, Z, raw_circuit='K', initial_guess=[0.01, 1], method='cost',
+       max_f=np.inf, max_M=20, tol=1e-5, plot=False, CI_plot=False):
+    '''
+    Wrapper function to perform measurement models fitting with either cost
+    or confidence interval method.
 
     Parameters:
+    -----------
     f : array-like
         Frequency data
     Z : array-like
-        2nd-NLEIS Impedance data
+        Impedance data
     raw_circuit: str
         Basis function for the measurement model
     initial_guess : list
-        Initial guess for model parameters
+        Initial guess for the basis model parameters
+    method : str
+        The method to use for fitting. Either 'cost' or 'conf'
+        Default: 'conf', which reproduces
+        the well-known measurement model program created by Mark Orazem. [1]
+        Convergence is not guaranteed
+        with the 'conf' method.
+        When 'conf' method fails, it is recommended to use the 'cost' method,
+        which is developed us and can be found in ref [2].
+
+    max_f : float
+        Maximum frequency cutoff
+        Default = inf
+    max_M : int
+        Maximum number of elements to try in the measurement model
+        Default = 20
     tol : float
         Tolerance for cost convergence
         Default: 1e-5
     plot : bool
         Whether to plot the results during the fitting process
         Default = False
+    CI_plot : bool
+        Whether to plot the 95% confidence interval calculated using
+        monte carlo simulation with random sampling of 1000 sample
+        using covariance derived standard deviation.
+        Default = False
+
+    Returns:
+    --------
+    M : int
+        The optimal number of elements determined by the fitting
+    cost : list
+        List of cost values for each iteration.
+        Only avaliable when method is 'cost'
+    p : list
+        Final optimized parameters
+    Z_fit : array
+        Fitted impedance values
+    res_real : array
+        Residuals for the real part of the impedance
+    res_imag : array
+        Residuals for the imaginary part of the impedance
+    conf : array
+        confidence interval of the fitted parameter calculated from covariance
+
+    Note:
+    -----
+    When method = 'cost': a total of 7 outputs are returned.
+    When method = 'conf': a total of 6 outputs are returned.
+
+    [1] Orazem, M.E. Measurement model for analysis of
+    electrochemical impedance data.
+    J Solid State Electrochem 28, 1273–1289 (2024).
+    https://doi.org/10.1007/s10008-023-05755-9
+
+    [2] Y. Ji, A. H. Shih D. T. Schwartz
+
+    '''
+
+    if method == 'cost':
+        return MM_cost(f, Z, raw_circuit, initial_guess, max_f, max_M, tol,
+                       plot)
+    elif method == 'conf':
+        return MM_conf(f, Z, raw_circuit, initial_guess, max_f, max_M,
+                       plot, CI_plot)
+    else:
+        raise ValueError('The method should be either cost or conf')
+
+
+def MM_cost(f, Z, raw_circuit='Kn', initial_guess=[0.01, 1],
+            max_f=np.inf, max_M=20, tol=1e-5,
+            plot=False):
+    """
+    Perform NLEIS fitting using nonlinear measurement models
+    with a specified maximum number of elements.
+
+    Parameters:
+    -----------
+    f : array-like
+        Frequency data
+    Z : array-like
+        Impedance data
+    raw_circuit: str
+        Basis function for the measurement model
+    initial_guess : list
+        Initial guess for model parameters
     max_f : float
         Maximum frequency cutoff
-        Default = 10 Hz
+        Default = inf
     max_M : int
         Maximum number of elements to try in the measurement model
         Default = 20
+    tol : float
+        Tolerance for cost convergence
+        Default: 1e-5
+    plot : bool
+        Whether to plot the results during the fitting process
+        Default = False
 
     Returns:
+    --------
     M : int
         The number of elements used in the circuit model
     cost : list
@@ -48,34 +135,43 @@ def NL_MM(f, Z, raw_circuit='Kn', initial_guess=[0.01, 1], tol=1e-5,
     res_imag : array
         Residuals for the imaginary part of the impedance
     conf : array
-        confidence interval of the fitted paramer calculated from covariance
+        confidence interval of the fitted parameter calculated from covariance
 
     """
+    if not isinstance(max_M, int):
+        raise TypeError(
+            'The maximum number of elements (max_M) should be an integer')
     if max_M < 1:
         raise ValueError(
             'The maximum number of elements (max_M) '
             + 'should always be greater than or equal to 1')
-
+    # Initialize the circuit model and initial guess
     circuit = raw_circuit+'0'
     p0 = initial_guess * max_M
     n = len(initial_guess)
+    # Initialize the cost
     cost = [0]
     previous_cost = 0
+    # Mask the data
     mask = f < max_f
     f = f[mask]
     Z = Z[mask]
+    # Main loop to fit the model
     with tqdm(total=max_M, file=None) as pbar:
         for i in range(max_M):
+            # perform MM fitting with M elements
             M = i + 1
             model = NLEISCustomCircuit(circuit, initial_guess=p0[:n*M])
             model.fit(f, Z, max_f=max_f)
             p0[:n*M] = model.parameters_
 
-            Z_fit = model.predict(f)
+            Z_fit = model.predict(f, max_f=max_f)
 
+            # Compute the cost
             current_cost = cost_max_norm(Z, Z_fit)
             cost.append(current_cost)
 
+            # generate plot for each iteration
             if plot:
                 fig, ax = plt.subplots(figsize=(5, 5))
                 plot_second(ax, Z)
@@ -93,7 +189,9 @@ def NL_MM(f, Z, raw_circuit='Kn', initial_guess=[0.01, 1], tol=1e-5,
                 plt.legend(['Data', 'Fit'])
                 plt.show()
 
+            # Check for convergence
             if abs(current_cost - previous_cost) < tol:
+                # If the cost is less than the tolerance, return the results
                 print(f'Optimal solution found with M = {M}')
 
                 res_real = (Z.real - Z_fit.real) / Z.real
@@ -107,23 +205,24 @@ def NL_MM(f, Z, raw_circuit='Kn', initial_guess=[0.01, 1], tol=1e-5,
                 conf = model.conf_
                 return M, cost[1:], p, Z_fit, res_real, res_imag, conf
 
+            # Update the cost and the circuit model
             previous_cost = current_cost
 
             circuit += '-' + raw_circuit + str(i+1)
             pbar.update(1)
 
-    raise ValueError(f'The calculation exceeded max_M = {max_M} without '
-                     + 'finding an optimal solution. '
-                     f'Final cost difference = {abs(cost[-1] - cost[-2]):.2e}.'
-                     + ' Please adjust max_M or other relevant parameters.')
+    raise ValueError(f'Exceeded max_M without finding an optimal solution. '
+                     f'Final cost difference: {abs(cost[-1] - cost[-2]):.2e}. '
+                     'Please adjust max_M or other parameters.')
 
 
-def LinMM(f, Z, raw_circuit='K', initial_guess=[0.01, 1],
-          plot=False, CI_plot=False,  max_M=20):
+def MM_conf(f, Z, raw_circuit='K', initial_guess=[0.01, 1],
+            max_f=np.inf, max_M=20,
+            plot=False, CI_plot=False):
     """
-    Perform EIS fitting using measurement models
+    Perform impedance fitting using measurement models
     with a specified maximum number of elements.
-    The LinMM function is aiming to reproduce
+    The MM_conf function is aiming to reproduce
     the well-known measurement model program created by Mark Orazem, [1]
     aiming to provide an easy accessible python API to do the same analysis.
 
@@ -141,6 +240,12 @@ def LinMM(f, Z, raw_circuit='K', initial_guess=[0.01, 1],
         Basis function for the measurement model
     initial_guess : list
         Initial guess for model parameters
+    max_f : float
+        Maximum frequency cutoff
+        Default = inf
+    max_M : int
+        Maximum number of elements to try in the measurement model
+        Default = 20
     plot : bool
         Whether to plot the results during the fitting process
         Default = False
@@ -149,9 +254,6 @@ def LinMM(f, Z, raw_circuit='K', initial_guess=[0.01, 1],
         monte carlo simulation with random sampling of 1000 sample
         using covariance derived standard deviation.
         Default = False
-    max_M : int
-        Maximum number of elements to try in the measurement model
-        Default = 20
 
     Returns:
     M : int
@@ -165,17 +267,27 @@ def LinMM(f, Z, raw_circuit='K', initial_guess=[0.01, 1],
     res_imag : array
         Residuals for the imaginary part of the impedance
     conf : array
-        confidence interval of the fitted paramer calculated from covariance
+        confidence interval of the fitted parameter calculated from covariance
 
     """
+    if not isinstance(max_M, int):
+        raise TypeError(
+            'The maximum number of elements (max_M) should be an integer')
     if max_M < 1:
         raise ValueError(
             'The maximum number of elements (max_M) '
             + 'should always be greater than or equal to 1')
 
+    # Initialize the circuit model and initial guess
     circuit = raw_circuit+'0'
     p0 = initial_guess * max_M
     n = len(initial_guess)
+    # Mask the data
+    mask = f < max_f
+    f = f[mask]
+    Z = Z[mask]
+
+    # Initialize the previous values
     Z_fit_previous = 0
     p_previous = 0
     conf_previous = 0
@@ -183,15 +295,16 @@ def LinMM(f, Z, raw_circuit='K', initial_guess=[0.01, 1],
 
     with tqdm(total=max_M, file=None) as pbar:
         for i in range(max_M):
-
+            # perform MM fitting with M elements
             M = i + 1
 
-            model = CustomCircuit(circuit, initial_guess=p0[:n*M])
+            model = NLEISCustomCircuit(circuit, initial_guess=p0[:n*M])
 
-            model.fit(f, Z)
+            model.fit(f, Z, max_f=max_f)
 
-            Z_fit = model.predict(f)
+            Z_fit = model.predict(f, max_f=max_f)
 
+            # calculate confidence intervals
             p_current = model.parameters_
             conf_current = model.conf_
             conf_intervals = [(p - 2*conf, p + 2*conf)
@@ -200,7 +313,14 @@ def LinMM(f, Z, raw_circuit='K', initial_guess=[0.01, 1],
             # Check if any parameter's confidence interval includes zero
             # if it includes zero, return results from previous calculation
             if any(lb <= 0 <= ub for lb, ub in conf_intervals):
+                if M-1 == 0:
+                    raise ValueError("'conf' method failed to find a solution"
+                                     + ' with CI does not include zero with '
+                                     + 'single basis function.'
+                                     + " Please try 'cost' method.")
+
                 print(f'Optimal solution found with M = {M-1}')
+
                 res_real = (Z.real - Z_fit_previous.real) / Z.real
                 res_imag = (Z.imag - Z_fit_previous.imag) / Z.imag
                 pbar.total = M-1
@@ -209,28 +329,33 @@ def LinMM(f, Z, raw_circuit='K', initial_guess=[0.01, 1],
 
                 if CI_plot:
                     # Monte Carlo simulation for confidence intervals
+                    # with random sampling of 1000 samples
                     n_simulations = 1000
+
+                    # initialize monte carlo simulation array and model
                     monte_simulation = np.zeros(
                         (n_simulations, len(f)), dtype='complex128')
 
-                    model = CustomCircuit(
+                    model = NLEISCustomCircuit(
                         circuit_previous, initial_guess=p_previous)
                     len_p = len(p_previous)
 
+                    # Perform Monte Carlo simulation
                     for i in range(n_simulations):
                         # Generate random parameters from a normal distribution
                         p1 = p_previous + \
                             np.random.normal(0, conf_previous, size=len_p)
 
                         model.initial_guess = p1
-                        monte_simulation[i] = model.predict(f)
+                        monte_simulation[i] = model.predict(f, max_f=max_f)
 
                     # Calculate confidence intervals
-                    # 95.4% confidence (lower)
-                    lower_bound = np.percentile(monte_simulation, 2.3, axis=0)
-                    # 95.4% confidence (upper)
-                    upper_bound = np.percentile(monte_simulation, 97.7, axis=0)
+                    # 95% confidence (lower)
+                    lower_bound = np.percentile(monte_simulation, 2.5, axis=0)
+                    # 95% confidence (upper)
+                    upper_bound = np.percentile(monte_simulation, 97.5, axis=0)
 
+                    # Plot the results
                     fig, ax = plt.subplots(figsize=(5, 5))
 
                     # plot data and fit
@@ -244,6 +369,8 @@ def LinMM(f, Z, raw_circuit='K', initial_guess=[0.01, 1],
                     plt.legend(['Data', 'Fit', '95% CI'])
 
                     plt.show()
+
+                # return the results
                 M = M-1
                 p = p_previous
                 Z_fit = Z_fit_previous
@@ -252,11 +379,13 @@ def LinMM(f, Z, raw_circuit='K', initial_guess=[0.01, 1],
                 return (M, p, Z_fit, res_real, res_imag,
                         conf)
 
+            # Update the previous values
             p_previous = model.parameters_
             conf_previous = model.conf_
             Z_fit_previous = Z_fit
             circuit_previous = circuit
 
+            # generate plot for each iteration
             if plot:
                 fig, ax = plt.subplots(figsize=(5, 5))
                 plot_first(ax, Z)
